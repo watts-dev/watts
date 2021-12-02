1443455d391b50a42efb2b4bc09c4e1418db00ea
Stauff <nstauff@anlvpn246-163.ne.anl.gov>
2021-12-02 13:08:01 -0600

    #2 improved ARDENT example for SAM.

--- examples/initial_OpenMC_SAM/GCMR_ardent.py
+++ examples/initial_OpenMC_SAM/GCMR_ardent.py
@@ -51,6 +51,9 @@ model['pf'] = 40 # percent
 # SAM Workflow
 
 sam_plugin = ardent.PluginSAM('sam_template')
+sam_plugin.sam_tmp_folder = "tmp_SAM"
+sam_exec = "../../sam-opt-mpi"  # Path to SAM executable
+sam_option = sam_plugin.options(SAM_exec=sam_exec)
 sam_plugin.workflow(model)#, sam_options)
 model.show_summary()
 
