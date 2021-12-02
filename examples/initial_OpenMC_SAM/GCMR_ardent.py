915a1c9bd09b1e30bcc24a3683061aefa819928a
Stauff <nstauff@anlvpn246-101.ne.anl.gov>
2021-12-02 17:29:23 -0600

    #2 improved ARDENT example for SAM.

--- examples/initial_OpenMC_SAM/GCMR_ardent.py
+++ examples/initial_OpenMC_SAM/GCMR_ardent.py
@@ -47,7 +47,7 @@ params['num_cpu'] = 60
 # SAM Workflow
 
 sam_plugin = ardent.PluginSAM('sam_template')
-sam_plugin._sam_exec = "../../sam-opt-mpi"
+sam_plugin._sam_exec = "/home/nstauff/ARDENT/examples/sam-opt-mpi"
 sam_result = sam_plugin.workflow(params)#, sam_options)
 params.show_summary()
 
