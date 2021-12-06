bbfdf0eb41286560deec2c328a0430f5a0a44293
Stauff <nstauff@anlvpn246-247.ne.anl.gov>
2021-12-06 08:31:33 -0600

    updated simple sam example

--- examples/initial_SAM/sam_example.py
+++ examples/initial_SAM/sam_example.py
@@ -45,20 +45,6 @@ params['num_cpu'] = 60
 # SAM Workflow
 
 sam_plugin = ardent.PluginSAM('sam_template')
-sam_exec = "../../sam-opt-mpi"  # Path to SAM executable - absolute or relative to temp file.
-sam_option = sam_plugin.options(SAM_exec=sam_exec)
+sam_plugin._sam_exec = "/home/rhu/projects/SAM/sam-opt"
+sam_result = sam_plugin.workflow(params)#, sam_options)
 
-sam_plugin.workflow(model)#, sam_options)
-model.show_summary()
-
-# get temperature from SAM results
-# model['temp'] = model['avg_Tgraphite'][-1]
-# # Run OpenMC plugin
-# openmc_plugin = ardent.PluginOpenMC(build_openmc_model)
-# openmc_plugin.workflow(model)
-
-sam_plugin.workflow(params)#, sam_options)
-params.show_summary()
-
-# Save results
-params.save('gcmr_sam.h5')
