1dc9852614ff717fc91f1a98e54d462280823a08
Nicolas Stauff <67026396+nstauff@users.noreply.github.com>
2022-01-21 15:12:29 -0600

    Update example2.py
    fixed link to SAM input

--- examples/example2_SAM_OpenMC/example2.py
+++ examples/example2_SAM_OpenMC/example2.py
@@ -59,7 +59,7 @@ params.show_summary(show_metadata=True, sort_by='time')
 
 moose_app_type = "SAM"
 app_dir = os.environ[moose_app_type.upper() + "_DIR"]
-sam_plugin = watts.PluginMOOSE('../example1_SAM/sam_template', show_stderr=True) # show only error
+sam_plugin = watts.PluginMOOSE('../example1a_SAM/sam_template', show_stderr=True) # show only error
 sam_plugin.moose_exec = app_dir + "/" + moose_app_type.lower() + "-opt"
 sam_result = sam_plugin.workflow(params)
 for key in sam_result.csv_data:
