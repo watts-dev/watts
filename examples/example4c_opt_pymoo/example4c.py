532982a35f2876b8f93a71c50c9faec712ab8433
Stauff <nstauff@anlvpn246-096.ne.anl.gov>
2022-01-30 21:45:33 -0600

    updated example 4c

--- examples/example4c_opt_pymoo/example4c.py
+++ examples/example4c_opt_pymoo/example4c.py
@@ -16,13 +16,12 @@ class fitness_calc(Problem):
         super().__init__(n_var=2, n_obj=2, xl=np.array([0.5, 0.5]), xu=np.array([1.0, 1.0]))
 
     def _evaluate(self, x, out, *args, **kwargs):
-        (keff, max_Tf, avg_Tf) = calc_workflow(x)
-        out["keff"] = keff
-        out["temp"] = max_Tf/avg_Tf
+        (keff, max_Tf, avg_Tf) =  (x[0], x[1], x[0] + x[1])# calc_workflow(x)
+        out["F"] = [keff, max_Tf/avg_Tf]
 
 algorithm = NSGA2(pop_size=5)
 
-res = minimize(fitness_calc(n_var=2, n_obj=2, xl=np.array([0.5, 0.5]), xu=np.array([1.0, 1.0])),
+res = minimize(fitness_calc(),
                algorithm,
                ('n_gen', 3),
                seed=1,
