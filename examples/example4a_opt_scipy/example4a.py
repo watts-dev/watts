970057f0ae52d5fdfdf7c26d7d977f67edb1eec1
Nicolas Stauff <67026396+nstauff@users.noreply.github.com>
2022-01-31 12:50:09 -0600

    Update examples/example4a_opt_scipy/example4a.py
    Co-authored-by: Paul Romano <paul.k.romano@gmail.com>

--- examples/example4a_opt_scipy/example4a.py
+++ examples/example4a_opt_scipy/example4a.py
@@ -11,7 +11,7 @@ def fitness_calc(X):
     """simple fitness function to get 1 "objective function" to scipy"""
     (keff, max_Tf, avg_Tf) = calc_workflow(X)
     fitness = abs(keff - 1) + (max_Tf/avg_Tf)
-    return keff
+    return fitness
 
 # optimization function - only 10 maximum iterations to make it run quick!
 res = minimize(fitness_calc, X, method ='SLSQP', bounds=((0.5, 1.0), (0.5, 0.99)), options={'maxiter': 10, 'iprint': 1, 'disp': False, 'eps': 0.01})
