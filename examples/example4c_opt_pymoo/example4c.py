3cc4d78fc61999c445d6470862dc8e3292d190f6
Stauff <nstauff@anlvpn246-096.ne.anl.gov>
2022-01-30 21:27:46 -0600

    updated example 4c

--- examples/example4c_opt_pymoo/example4c.py
+++ examples/example4c_opt_pymoo/example4c.py
@@ -2,20 +2,27 @@
 # SPDX-License-Identifier: MIT
 
 from example4 import *
+import numpy as np
 from pymoo.algorithms.moo.nsga2 import NSGA2
 from pymoo.optimize import minimize
+from pymoo.problems.functional import FunctionalProblem
+from pymoo.core.problem import Problem
 
 # initial X values
-X = [0.9, 0.6]
 
-def fitness_calc(X): # simple fitness function to get 1 "objective function" to scipy
-    (keff, max_Tf, avg_Tf) = calc_workflow(X)
-    fitness = (abs(keff - 1), (max_Tf/avg_Tf))
-    return fitness
+# Problem definition
+class fitness_calc(Problem):
+    def __init__(self):
+        super().__init__(n_var=2, n_obj=2, xl=np.array([0.5, 0.5]), xu=np.array([1.0, 1.0]))
+
+    def _evaluate(self, x, out, *args, **kwargs):
+        (keff, max_Tf, avg_Tf) = calc_workflow(x)
+        out["keff"] = keff
+        out["temp"] = max_Tf/avg_Tf
 
 algorithm = NSGA2(pop_size=5)
 
-res = minimize(fitness_calc,
+res = minimize(problem,
                algorithm,
                ('n_gen', 3),
                seed=1,
