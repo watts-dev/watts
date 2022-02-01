67db81d380d6b8a59d5057658176cc1380084850
Nicolas Stauff <nstauff@anl.gov>
2022-02-01 08:47:32 -0600

    updated problem definition to properly store results in pymoo

--- examples/example4c_opt_pymoo/example4c.py
+++ examples/example4c_opt_pymoo/example4c.py
@@ -5,21 +5,19 @@ from example4 import *
 import numpy as np
 from pymoo.algorithms.moo.nsga2 import NSGA2
 from pymoo.optimize import minimize
-from pymoo.core.problem import Problem
+from pymoo.core.problem import ElementwiseProblem
 
 # Problem definition
-class fitness_calc(Problem):
+class fitness_calc(ElementwiseProblem):
     """simple definition of the optimization problem with initialization and evaluation methods"""
     def __init__(self):
         super().__init__(n_var=2, n_obj=2, xl=np.array([0.5, 0.5]), xu=np.array([1.0, 1.0])) # initialization of the problem
 
     def _evaluate(self, x, out, *args, **kwargs):
         print(x) # this is the matrix of the size of the population
-        out["F"] = [] 
-        for s in x:
-            (keff, max_Tf, avg_Tf) = calc_workflow(s) # the workflow is called here and we are saving the results in out['F']
-            print(keff, max_Tf, avg_Tf) 
-            out["F"].append([keff, max_Tf/avg_Tf]) 
+        (keff, max_Tf, avg_Tf) = calc_workflow(s) # the workflow is called here and we are saving the results in out['F']
+        print(keff, max_Tf, avg_Tf) 
+        out["F"] = [keff, max_Tf/avg_Tf]
 
 algorithm = NSGA2(pop_size=2) # multicriteria algorithm applied
 
@@ -28,5 +26,5 @@ res = minimize(fitness_calc(),
                ('n_gen', 2),
                seed=1,
                verbose=False) # this runs the optimization algorithm
-print("all variables", res.x)
+print("all variables", res.X)
 print("all results", res.F)
