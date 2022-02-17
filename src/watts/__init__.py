9b5a5cdfabd7b1e1465cfb5d5f1ada283465a8e8
Paul Romano <promano@anl.gov>
2022-02-17 17:41:43 -0600

    Provide a watts.Quantity alias

--- src/watts/__init__.py
+++ src/watts/__init__.py
@@ -8,3 +8,6 @@ from .plugin_pyarc import *
 from .template import *
 from .parameters import *
 from .database import *
+
+# This allows a user to write watts.Quantity
+from astropy.units import Quantity
