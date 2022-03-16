1b7f64842d72cb296eb8c6a61980b2fc9df9a2d2
Zhiee Jhia Ooi <35579376+zhieejhia93@users.noreply.github.com>
2022-03-16 08:35:04 -0500

    Update examples/example1d_SAM_ParametricStudy/example1d.py
    Co-authored-by: Paul Romano <paul.k.romano@gmail.com>

--- examples/example1d_SAM_ParametricStudy/example1d.py
+++ examples/example1d_SAM_ParametricStudy/example1d.py
@@ -61,9 +61,9 @@ for i in range(len(power)):
     moose_result = moose_plugin(params)
 
     # Add items to dictionary.
-    results_dict['time_'+str(i+1)] = moose_result.csv_data['time']
-    results_dict['max_Tcoolant_'+str(i+1)] = moose_result.csv_data['max_Tcoolant']
-    results_dict['max_Tw_'+str(i+1)] = moose_result.csv_data['max_Tw']
+    results_dict[f'time_{i+1}'] = moose_result.csv_data['time']
+    results_dict[f'max_Tcoolant_{i+1}'] = moose_result.csv_data['max_Tcoolant']
+    results_dict[f'max_Tw_{i+1}'] = moose_result.csv_data['max_Tw']
 
 df = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in results_dict.items()])) # Store dictionary items as dataframe. Columns of unequal lengths are padded with NaN.
 df.reindex(sorted(df.columns), axis=1).to_csv('results.csv') # Sort column names alphabetically and save as CSV file.
