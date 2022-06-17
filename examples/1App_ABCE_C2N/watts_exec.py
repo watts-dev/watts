d891320f131f3b9750a80b9a509ca1a3fdd34498
sdotson <sdotson@anl.gov>
2022-06-17 13:51:06 -0500

    init commit for the ABCE plugin example exec file

---
+++ examples/1App_ABCE_C2N/watts_exec.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2022 UChicago Argonne, LLC
+# SPDX-License-Identifier: MIT
+
+"""
+This example demonstrates how to use WATTS to run an
+ABCE calculation. This example uses a coal-to-nuclear transition
+scenario.
+"""
+
+import watts
+import os
+import pathlib
