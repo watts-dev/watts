f4a3b69bb968445cc69aeb5117b3bed619631089
sdotson <sdotson@anl.gov>
2022-06-20 15:15:35 -0500

    adds basic workflow for ABCE runs (note: does not appear to actually run ABCE)

--- examples/1App_ABCE_C2N/watts_exec.py
+++ examples/1App_ABCE_C2N/watts_exec.py
@@ -10,3 +10,32 @@ scenario.
 import watts
 import os
 import pathlib
+import pandas as pd
+import numpy as np
+
+params = watts.Parameters()
+
+params['DATABASE_NAME'] = 'test.db'
+
+params.show_summary(show_metadata=True, sort_by='key')
+
+
+average_ngp = 5  # $/mmbtu
+variance_ngp = 2
+n_samples = 10
+
+np.random.seed(12345)
+ngp_list = np.random.normal(loc=average_ngp, scale=variance_ngp, size=n_samples) # $/mmbtu
+ptc_list = np.linspace(start=0, stop=30, num=n_samples) # $/MWh
+
+results_dict = {}
+
+for i,n in enumerate(ngp_list): # loop through all of the natural gas prices
+    params['NATURAL_GAS_PRICE'] = n
+    for j,p in enumerate(ptc_list):
+        params['PTC_VALUE'] = p
+
+        params.show_summary(show_metadata=True, sort_by='key')
+
+        abce_plugin = watts.PluginABCE('abce_template.txt')
+        abce_result = abce_plugin(params)
