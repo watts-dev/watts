cc731ac51ada6ee3f3f427bc2ebfba816f2938cd
Zhiee Jhia Ooi <zooi@anl.gov>
2022-07-06 15:45:18 -0500

    Updated example

--- examples/Optimization_PyARC_DAKOTA/dakota_driver_ori.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-import watts
-import sys
-sys.path.insert(0, '/software/Workbench/dakota/share/dakota/Python/dakota')
-DakotaDriver = watts.PluginDakotaDriver(coupled_code_exec='watts_pyarc_exec.py')
