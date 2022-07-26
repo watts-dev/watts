3bf732e69d410109c7351e90ed656b6eb957b11f
Sam Dotson <samgdotson@gmail.com>
2022-07-26 13:24:56 -0500

    updates example and input file

--- examples/1App_ABCE_C2N/watts_exec.py
+++ examples/1App_ABCE_C2N/watts_exec.py
@@ -16,9 +16,11 @@ import numpy as np
 
 params = watts.Parameters()
 
+n_steps = 2
 results_path = Path.cwd() / 'results'
 results_path.mkdir(exist_ok=True)
-params['PTC_VALUE'] = 25.0
+params['NFOM_VALUE'] = 'ATB'
+params['N_STEPS'] = n_steps
 
 watts.Database.set_default_path(results_path)
 
@@ -26,21 +28,20 @@ watts.Database.set_default_path(results_path)
 
 average_ngp = 5  # $/mmbtu
 variance_ngp = 2
-n_samples = 2
+n_samples = 4
 
 np.random.seed(12345)
 # ngp_list = np.random.normal(loc=average_ngp, scale=variance_ngp, size=n_samples) # $/mmbtu
-ngp_list = np.linspace(start = 2.0, stop=30, num=n_samples) # $/mmbtu
-nfom_list = np.linspace(start=40, stop=500, num=n_samples) # $/MWh
-
+ngp_list = np.linspace(start = 0.5, stop=30, num=n_samples) # $/mmbtu
+# nfom_list = np.linspace(start=40, stop=500, num=n_samples) # $/MWh
+ptc_list = np.linspace(start=0, stop=60, num=n_samples) # $/MWh
 results_dict = {}
 
 for i,n in enumerate(ngp_list): # loop through all of the natural gas prices
     params['NATURAL_GAS_PRICE'] = n
-    for j,p in enumerate(nfom_list):
-        params['NFOM_VALUE'] = p
-        params['DATABASE_NAME'] = f'NG_NFOM_run_0{i}{j}.db'
+    for j,p in enumerate(ptc_list):
+        params['PTC_VALUE'] = p
+        params['DATABASE_NAME'] = f'NG_PTC_run_1{i}{j}_pd{n_steps}.db'
         params.show_summary(show_metadata=True, sort_by='key')
-        # breakpoint()
         abce_plugin = watts.PluginABCE('abce_template.txt', show_stdout=True, show_stderr=True)
         abce_result = abce_plugin(params, extra_args=['-f'])
