8d1ae5853fa4918d2d7d2698f7c1b4ae72137bd4
Paul Romano <paul.k.romano@gmail.com>
2023-04-19 14:43:53 -0500

    Account for ACE table availability in element expansion

--- src/watts/plugin_mcnp.py
+++ src/watts/plugin_mcnp.py
@@ -1,8 +1,10 @@
 # SPDX-FileCopyrightText: 2022-2023 UChicago Argonne, LLC
 # SPDX-License-Identifier: MIT
 
+import os
 import re
-from typing import List, Optional
+from pathlib import Path
+from typing import List, Optional, Dict, Tuple
 
 from uncertainties import ufloat
 
@@ -15,13 +17,16 @@ from .results import Results
 def expand_element(value, default_suffix=None):
     values = value.split()
 
+    # TODO: Allow xsdir to be specified
+    available_tables = get_tables_from_xsdir()
+
     # Determine whether filter block includes start of material card
     # TODO: Use regex to break into multiple material blocks?
     if len(values) % 2 == 1:
         mat_num, *values = values
         start = f"{mat_num}    "
     else:
-        start = ""
+        start = "     "
 
     lines = []
     for zaid_with_suffix, conc in zip(values[::2], values[1::2]):
@@ -43,13 +48,104 @@ def expand_element(value, default_suffix=None):
         if A == 0:
             conc = float(conc)
             symbol = ATOMIC_SYMBOL[Z]
-            for isotope, fraction in isotopes(symbol):
+
+            # Determine what isotopes to add
+            available_isotopes = available_tables[Z, suffix]
+            natural_isotope_fractions = isotopes(symbol)
+            natural_isotopes = [isotope for isotope, _ in natural_isotope_fractions]
+            missing_isotopes = set(natural_isotopes) - set(available_isotopes)
+            if len(available_isotopes) == 1:
+                # Case 1 -- only a single isotope available. In this case, all
+                # the concentration goes to that single isotope
+                isotope_fractions = [(available_isotopes[0], 1.0)]
+            elif len(missing_isotopes) == 0:
+                # Case 2 -- all isotopes are available, use as is!
+                isotope_fractions = natural_isotope_fractions
+            elif len(missing_isotopes) == 1:
+                # Case 3 -- one missing isotope. In this case, lump it into
+                # whatever natural isotope has the highest abundance
+
+                # Determine which isotope has highest abundance
+                highest_item = max(natural_isotope_fractions, key=lambda x: x[1])
+                highest_index = natural_isotope_fractions.index(highest_item)
+
+                # Determine index/fraction of missing isotope
+                missing_item, = missing_isotopes
+                missing_index = natural_isotopes.index(missing_item)
+                missing_fraction = natural_isotope_fractions[missing_index][1]
+
+                # Replace missing isotope with highest abundance one
+                natural_isotope_fractions[highest_index] = (
+                    highest_item[0], highest_item[1] + missing_fraction)
+                natural_isotope_fractions.pop(missing_index)
+                isotope_fractions = natural_isotope_fractions
+            else:
+                raise ValueError(f"Could not expand {zaid}")
+
+            for isotope, fraction in isotope_fractions:
                 iso_A = int(*re.match(rf'{symbol}(\d+)', isotope).groups())
                 lines.append(f"{Z}{iso_A:03}.{suffix} {conc * fraction}")
         else:
             lines.append(f"{zaid_with_suffix} {conc}")
 
     return start + "\n     ".join(lines)
+
+
+def get_tables_from_xsdir(path: Optional[PathLike] = None) -> Dict[Tuple[int, str], List[str]]:
+    """Determine paths to ACE files from an MCNP xsdir file.
+
+    Parameters
+    ----------
+    path
+        Path to xsdir file
+
+    Returns
+    -------
+    dict
+        Dictionary mapping (Z, suffix) to a list of ZAID identifiers
+    """
+    if path is None:
+        datapath = os.environ.get('DATAPATH')
+        if datapath is None:
+            raise EnvironmentError(
+                "Need to set DATAPATH environment vairable to determine what "
+                "MCNP cross section libraries are available.")
+        path = Path(datapath) / 'xsdir'
+
+    # Find 'directory' section
+    with open(path, 'r') as fh:
+        lines = fh.readlines()
+    for index, line in enumerate(lines):
+        if line.strip().lower() == 'directory':
+            break
+    else:
+        raise RuntimeError("Could not find 'directory' section in MCNP xsdir file")
+
+    # Handle continuation lines indicated by '+' at end of line
+    lines = lines[index + 1:]
+    continue_lines = [i for i, line in enumerate(lines)
+                      if line.strip().endswith('+')]
+    for i in reversed(continue_lines):
+        lines[i] = lines[i].strip()[:-1] + lines.pop(i + 1)
+
+    # Create list of ACE libraries
+    tables = {}
+    for line in lines:
+        words = line.split()
+        if len(words) < 3:
+            continue
+
+        if not words[0].endswith('c'):
+            continue
+
+        zaid, suffix = words[0].split('.')
+        Z, A = divmod(int(zaid), 1000)
+        symbol = ATOMIC_SYMBOL[Z]
+        if (Z, suffix) not in tables:
+            tables[Z, suffix] = []
+        tables[Z, suffix].append(f'{symbol}{A}')
+
+    return tables
 
 
 class ResultsMCNP(Results):
